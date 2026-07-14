9dcdcc2beaf9ac9dc33b2bbba73fc09f86c1bbff
Maddie Savary <masa6707@colorado.edu>
2026-07-14 15:24:02 -0600

    Fixed the print labels to describe transactions

--- analysis.py
+++ analysis.py
@@ -1,4 +1,6 @@
 import pandas as pd
 df = pd.read_csv("Data.csv")
 print("Number of rows:", len(df))
-print("Mean price:", df["price"].mean())
+print("Mean transaction price:", df["price"].mean())
+# print the largest sales
+print("Largest transaction:", df["price"].max())
