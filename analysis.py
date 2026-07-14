924c0a7e3ad34299d582b7a0f2930e6e526aeaa4
Maddie Savary <masa6707@colorado.edu>
2026-07-14 15:18:21 -0600

    space holder?

--- analysis.py
+++ analysis.py
@@ -2,5 +2,3 @@ import pandas as pd
 df = pd.read_csv("Data.csv")
 print("Number of rows:", len(df))
 print("Mean price:", df["price"].mean())
-# print the largest sale:
-print("Largest sale:", df["price"].max())
